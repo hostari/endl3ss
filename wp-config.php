9b7710f51ab76042bafd48eec9ceca1364c53a82
Gia Galvez <gia@hostari.com>
2024-02-03 11:11:30 +0100

    updated config

--- wp-config.php
+++ wp-config.php
@@ -96,5 +96,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
 
-define( 'WP_HOME', 'https://example.com' );
-define( 'WP_SITEURL', 'https://example.com' );
+define( 'WP_HOME', 'https://endl3ss.hostari.com' );
+define( 'WP_SITEURL', 'https://endl3ss.hostari.com' );
