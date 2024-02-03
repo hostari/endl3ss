e161834d2d0857b4c0e0fe3fcb2213c3847accb4
Gia Galvez <gia@hostari.com>
2024-02-03 10:01:15 +0100

    add site url to config

--- wp-config.php
+++ wp-config.php
@@ -95,3 +95,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+define( 'WP_HOME', 'https://endl3ss.hostari.com/' );
+define( 'WP_SITEURL', 'https://endl3ss.hostari.com/' );
